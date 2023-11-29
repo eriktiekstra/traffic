bced90272f6debc3cc6bb1b7c9e767d2ac9f1fef
Erik Tiekstra <erik.tiekstra@weahead.se>
2023-11-29 09:32:20 +0100

    feat: pwa

--- app/root.jsx
+++ app/root.jsx
@@ -7,7 +7,7 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
-import styles from "./tailwind.css"
+import styles from "./tailwind.css";
 
 export function links() {
   return [{ rel: "stylesheet", href: styles }]
@@ -19,6 +19,15 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0372c0" />
+        <meta name="apple-mobile-web-app-title" content="SL Traffic" />
+        <meta name="application-name" content="SL Traffic" />
+        <meta name="msapplication-TileColor" content="#ff0000" />
+        <meta name="theme-color" content="#ffffff" />
         <Meta />
         <Links />
       </head>
